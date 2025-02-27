29954a0ba00cd9c8a360cfa3c7c1084198c6ceb2
harryhritik <hritikpankal1999@gmail.com>
2025-02-27 12:05:02 +0530

    change origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: ["https://home-decor-bice.vercel.app/"],
+      origin: ["https://home-decor-bice.vercel.app"],
       credentials: true,
     })
   );
